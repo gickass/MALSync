9cba6ad483e5e3d85262eaec021f483a86842ddc
Gickass <faheemmahidin@gmail.com>
2026-05-13 18:31:58 +0800

    wip

--- src/pages-chibi/implementations/UniqueStream/main.ts
+++ src/pages-chibi/implementations/UniqueStream/main.ts
@@ -1,10 +1,12 @@
+import { get } from 'jquery';
+import type { ChibiGenerator } from '../../../chibiScript/ChibiGenerator';
 import { PageInterface } from '../../pageInterface';
 
 export const UniqueStream: PageInterface = {
   name: 'UniqueStream',
   domain: 'https://anime.uniquestream.net',
   languages: ['English'],
-  type: 'manga',
+  type: 'anime',
   urls: {
     match: ['*://anime.uniquestream.net/*'],
   },
@@ -16,25 +18,82 @@ export const UniqueStream: PageInterface = {
         .run();
     },
     getTitle($c) {
-      const baseTitle = $c.querySelector('h1').ifNotReturn().text().trim().run();
+      const baseTitle = $c.querySelector('.episode-series-link').ifNotReturn().text().trim().run();
       return $c
         .if(
-          $c.querySelector('h2').equals('Season 1').run(),
-          baseTitle,
+          getSeasonCheckSync($c),
           $c
             .fn(baseTitle)
             .concat(' ')
-            .concat($c.querySelector('h2').ifNotReturn().text().trim().run())
+            .concat($c.title().regex('Season \\d+').ifNotReturn().trim().run())
+            .run(),
+          baseTitle,
+        )
+        .run();
+    },
+    getIdentifier($c) {
+      const baseIdentifier = $c.this('sync.getOverviewUrl').urlPart(5).run();
+      return $c
+        .if(
+          getSeasonCheckSync($c),
+          $c
+            .fn(baseIdentifier)
+            .concat('-')
+            .concat($c.title().regex('Season \\d+').ifNotReturn().trim().run())
             .slugify()
+            .run(),
+          baseIdentifier,
+        )
+        .run();
+    },
+    getOverviewUrl($c) {
+      return $c
+        .querySelector('.episode-series-link')
+        .ifNotReturn()
+        .getAttribute('href')
+        .urlAbsolute()
+        .run();
+    },
+    getEpisode($c) {
+      return $c.querySelector('episode-watch-title').text().regex(EpRegex).number().run();
+    },
+    nextEpUrl($c) {
+      return $c
+        .querySelectorAll('.np-label')
+        .arrayFind($text => $text.text().contains('Next Episode').run())
+        .ifNotReturn()
+        .closest('a')
+        .getAttribute('href')
+        .urlAbsolute()
+        .run();
+    },
+  },
+  overview: {
+    isOverviewPage($c) {
+      return $c
+        .and($c.url().urlPart(5).boolean().run(), $c.url().urlPart(3).equals('series').run())
+        .run();
+    },
+    getTitle($c) {
+      const baseTitle = $c.querySelector('.series-title').ifNotReturn().text().trim().run();
+      return $c
+        .if(
+          getSeasonCheckOverview($c),
+          baseTitle,
+          $c
+            .fn(baseTitle)
+            .ifNotReturn()
+            .concat(' ')
+            .concat($c.querySelector('h2').ifNotReturn().text().trim().run())
             .run(),
         )
         .run();
     },
     getIdentifier($c) {
-      const baseIdentifier = $c.this('sync.getOverviewUrl').this('overview.getIdentifier').run();
+      const baseIdentifier = $c.url().urlPart(5).run();
       return $c
         .if(
-          $c.querySelector('h2').equals('Season 1').run(),
+          getSeasonCheckOverview($c),
           baseIdentifier,
           $c
             .fn(baseIdentifier)
@@ -45,63 +104,22 @@ export const UniqueStream: PageInterface = {
         )
         .run();
     },
-    getOverviewUrl($c) {
-      return $c
-        .querySelector('.episode-series-link')
-        .ifNotReturn()
-        .getAttribute('href')
-        .urlPart(5)
-        .run();
-    },
-    getEpisode($c) {
-      return $c.url().urlPart(5).number().run();
-    },
-    nextEpUrl($c) {
-      return $c
-        .querySelector('option:checked')
-        .prev()
-        .ifNotReturn()
-        .string('/reader/<identifier>/')
-        .replace('<identifier>', $c.this('sync.getIdentifier').run())
-        .concat($c.querySelector('option:checked').prev().getAttribute('value').ifNotReturn().run())
-        .urlAbsolute()
-        .run();
-    },
-    readerConfig: [
-      {
-        current: $c => $c.querySelectorAll('.w-full img').countAbove().run(),
-        total: $c => $c.querySelectorAll('.w-full img').length().run(),
-      },
-    ],
-  },
-  overview: {
-    isOverviewPage($c) {
-      return $c
-        .and($c.url().urlPart(4).boolean().run(), $c.url().urlPart(3).equals('series').run())
-        .run();
-    },
-    getTitle($c) {
-      return $c.querySelector('h1').ifNotReturn().text().replaceLinebreaks().trim().run();
-    },
-    getIdentifier($c) {
-      return $c.url().urlPart(4).run();
-    },
     getImage($c) {
       return $c.querySelector('.object-cover').getAttribute('src').ifNotReturn().run();
     },
     uiInjection($c) {
-      return $c.querySelector('h1').parent().uiAfter().run();
+      return $c.querySelector('.series-title').uiBefore().run();
     },
   },
   list: {
     elementsSelector($c) {
-      return $c.querySelectorAll('.grid > a[href*="reader"]').run();
+      return $c.querySelectorAll('.place-content-start a').run();
     },
     elementUrl($c) {
       return $c.getAttribute('href').urlAbsolute().run();
     },
     elementEp($c) {
-      return $c.this('list.elementUrl').this('sync.getEpisode').number().run();
+      return $c.find('.episode-title').text().regex(EpRegex).number().run();
     },
   },
   lifecycle: {
@@ -109,15 +127,34 @@ export const UniqueStream: PageInterface = {
       return $c.addStyle(require('./style.less?raw').toString()).run();
     },
     ready($c) {
-      return $c.detectURLChanges($c.trigger().run()).domReady().trigger().run();
-    },
-    listChange($c) {
       return $c
+        .detectURLChanges($c.trigger().run())
         .detectChanges(
-          $c.querySelector('.grid > a[href*="reader"]').ifNotReturn().parent().run(),
+          $c.querySelector('[class*="season-dropdown"] h2').ifNotReturn().text().run(),
           $c.trigger().run(),
         )
+        .domReady()
+        .trigger()
         .run();
+    },
+    overviewIsReady($c) {
+      return $c
+        .waitUntilTrue($c.querySelector('.place-content-start').boolean().run())
+        .trigger()
+        .run();
+    },
+    syncIsReady($c) {
+      return $c.waitUntilTrue($c.querySelector('#videoContainer').boolean().run()).trigger().run();
     },
   },
 };
+
+function getSeasonCheckSync($c: ChibiGenerator<unknown>) {
+  return $c.title().matches('Season\\s*1|S\\s*1').log().run();
+}
+
+function getSeasonCheckOverview($c: ChibiGenerator<unknown>) {
+  return $c.querySelector('h2').text().matches('Season\\s*1|S\\s*1').log().run();
+}
+
+const EpRegex = 'E\\s*(\\d+)|Episode\\s*(\\d+)';
